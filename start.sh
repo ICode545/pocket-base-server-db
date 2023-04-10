4598f30332fc8b486b5ea1534f08efe1be3b3255
024x <satyendrabongi@gmail.com>
2023-04-10 21:17:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,3 @@ sudo apt install -y curl unzip
 curl -LOk https://github.com/pocketbase/pocketbase/releases/download/v0.14.3/pocketbase_0.14.3_linux_amd64.zip
 unzip pocketbase_0.14.3_linux_amd64.zip
 rm pocketbase_0.14.3_linux_amd64.zip
-cd /pocketbase_0.14.3_linux_amd64
-./pocketbase serve
